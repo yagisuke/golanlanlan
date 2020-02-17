ace7b57157b2749b0d3d1d98e3e3271b869f1871
yagisuke <yagisuke.0209@gmail.com>
2020-02-17 19:14:32 +0900

    fmt go

--- 03-fmt.go
+++ 03-fmt.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("火星の表面では、私の体重は %v キログラムです。\n", 62 * 0.3783)
-	fmt.Printf("年齢は %v 歳になるそうです。\n", 31 * 365 / 687)
+	fmt.Printf("%v で %v 歳の私は、\n", "地球", 31)
+	fmt.Printf("%v の表面では %v 歳になるそうです。\n", "火星", 31 * 365 / 687)
 }
