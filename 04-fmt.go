ebb2cf6eb0416c909e65712154044eb8ad61921c
yagisuke <yagisuke.0209@gmail.com>
2020-02-17 19:20:09 +0900

    fmt go

---
+++ 04-fmt.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%-15v |%4v\n", "MINUS MAX", -100)
+	fmt.Printf("%-15v |%4v\n", "MINUS MID", -49)
+	fmt.Printf("%-15v |%4v\n", "PLUS HIGHT", 94)
+	fmt.Printf("%-15v |%4v\n", "PLUS MAX", 100)
+}
