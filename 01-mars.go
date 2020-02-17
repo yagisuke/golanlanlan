844c088ef78eff74b2ce25a20855266826989bf8
yagisuke <yagisuke.0209@gmail.com>
2020-02-17 19:05:57 +0900

    mars go

--- 01-mars.go
+++ 01-mars.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Print("火星の表面では、私の体重は")
 	fmt.Print(62 * 0.3783)
-	fmt.Print("ポンド、年齢は")
+	fmt.Print("キログラム、年齢は")
 	fmt.Print(31 * 365 / 687)
 	fmt.Print("歳になるそうです。")
 }
---
+++ 02-mars.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("火星の表面では、私の体重は", 62 * 0.3783, "キログラム")
+	fmt.Print("年齢は", 31 * 365 / 687, "歳になるそうです。")
+}
