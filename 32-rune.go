4823d3629c1df2bef9a6ad839bb7f98656cdddf8
yagisuke <yagisuke.0209@gmail.com>
2020-02-28 20:26:08 +0900

    rune go

---
+++ 32-rune.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var pi rune = 960
+	var alpha rune = 940
+	var omega rune = 969
+	var bang rune = 33
+
+	fmt.Printf("%v %v %v %v\n", pi, alpha, omega, bang)
+	fmt.Printf("%c %c %c %c\n", pi, alpha, omega, bang)
+}
---
+++ 34-caesar.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	d := 'a'
+	d = d + 3
+	fmt.Printf("%c", d)
+}
